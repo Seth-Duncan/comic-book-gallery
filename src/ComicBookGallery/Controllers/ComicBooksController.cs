7885fc1c355756d2fee74dcb35c59abef28a1e70
Seth Duncan <seth@seth-duncan.com>
2017-10-28 10:50:31 -0700

    Added comic book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,13 +11,7 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult  Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello form the comic books controller!");
-
+            return View();
         }
 
     }
